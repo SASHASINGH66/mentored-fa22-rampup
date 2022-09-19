08b581c4bda24ce70a1bcb91ffcbefc6a6fb79ed
Sasha Singh <sashasingh@Sashas-MacBook-Pro.local>
2022-09-19 09:14:00 -0700

    can generate 3 random album covers, removed text entry boxes

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+
 function myFunction() {
-  alert("This is the correct way to do js ☺️ 😊🥰👉👈");
+  alert("Tranquility base hotel and casino is the right album!")
+    
 }
 
-function submit() {
-    username = document.getElementById('username');
-    email = document.getElementById('email');
-    password = document.getElementById('password');
-    alert('Username:', username.value, 'Email:', email.value, 'Password:', password.value);
-    return false;
+
+var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg");
+
+var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp");
+
+var pics3 = new Array("in_utero.jpg", "nevermind.webp", "meteora.jpg");
+
+var songs = new Array("love is a laserquest", "four out of five", "505", "r u mine", "mardy bum", "i bet u look good on the dancefloor", "heart shaped box", "lithium", "numb");
+
+function dispsong(){
+  var random = Math.floor(Math.random()*songs.length);
+  return songs[random];
 }
+function choosePic(img, p){
+  var random = Math.floor(Math.random()*p.length);
+  document.getElementById(img).src = p[random];
+}
+
+
+choosePic("image1", pics1);
+choosePic("image2", pics2);
+choosePic("image3", pics3);
