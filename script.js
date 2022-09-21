5bf8857c553ec807154db93fa25ca90ebab5a4e6
Sasha Singh <sashasingh@Sashas-MacBook-Pro.local>
2022-09-20 20:34:11 -0700

    changes border colours, correctly identifies right album, changed idea to finding out most popular album

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg", "divide.png");
+
+var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp", "red.png");
+
+var pics3 = new Array("in_utero.jpg", "nevermind.webp", "meteora.jpg", "fearless.png");
+
+var most_popular = new Array("in utero", "nevermind", "meteora", "red");
+
+var most_popular_ids = new Array("image3", "image3", "image3", "image2");
+
+var random = Math.floor(Math.random()*4);
 
 function myFunction() {
-  alert("Tranquility base hotel and casino is the right album!")
-    
+  var most_pop = most_popular[random];
+  var element = document.getElementById(most_popular_ids[random]);
+  element.style.borderColor = "#00FF00";
+  alert(most_pop + ' was the most popular');
+
 }
 
-
-var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg");
-
-var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp");
-
-var pics3 = new Array("in_utero.jpg", "nevermind.webp", "meteora.jpg");
-
-var songs = new Array("love is a laserquest", "four out of five", "505", "r u mine", "mardy bum", "i bet u look good on the dancefloor", "heart shaped box", "lithium", "numb");
-
-function dispsong(){
-  var random = Math.floor(Math.random()*songs.length);
-  return songs[random];
-}
 function choosePic(img, p){
-  var random = Math.floor(Math.random()*p.length);
   document.getElementById(img).src = p[random];
 }
 
-
+function submit(){
+  var e = document.getElementById("period");
+  var value = e.value;
+  
+}
 choosePic("image1", pics1);
 choosePic("image2", pics2);
 choosePic("image3", pics3);
