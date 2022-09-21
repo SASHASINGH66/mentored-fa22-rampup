83d7be6aeeb7a7dcc8aefc62616a0f671fb17335
Sasha Singh <sashasingh@Sashas-MacBook-Pro.local>
2022-09-20 23:04:40 -0700

    added audio

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg", "divide.png");
 
 var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp", "red.png");
@@ -8,25 +9,24 @@ var most_popular = new Array("in utero", "nevermind", "meteora", "red");
 
 var most_popular_ids = new Array("image3", "image3", "image3", "image2");
 
+var most_popular_sounds = new Array("heart_box.mp3", "nirvana.mp3", "numb.mp3", "red.mp3")
+
 var random = Math.floor(Math.random()*4);
-
-function myFunction() {
-  var most_pop = most_popular[random];
-  var element = document.getElementById(most_popular_ids[random]);
-  element.style.borderColor = "#00FF00";
-  alert(most_pop + ' was the most popular');
-
-}
 
 function choosePic(img, p){
   document.getElementById(img).src = p[random];
 }
 
-function submit(){
-  var e = document.getElementById("period");
-  var value = e.value;
-  
+function myFunction() {
+  var most_pop = most_popular[random];
+  var most_pop_sound = most_popular_sounds[random];
+  var element = document.getElementById(most_popular_ids[random]);
+  element.style.borderColor = "#00FF00";
+  var snd = new Audio(most_popular_sounds[random]);
+  snd.play();
+  alert(most_pop + ' was the most popular');
 }
+
 choosePic("image1", pics1);
 choosePic("image2", pics2);
 choosePic("image3", pics3);
