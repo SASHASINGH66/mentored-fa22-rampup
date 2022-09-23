a11d981862d3dc2880d6d3bdbe309a630c0ee560
Sasha Singh <sashasingh@Sashas-MacBook-Pro.local>
2022-09-22 19:18:40 -0700

    finished project

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 
-var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg", "divide.png");
+var pics1 = new Array("Suckitandsee.jpg", "tbhc.webp", "Favourite_Worst_Nightmare.jpeg", "divide.png", "evolve.jpeg", "happier_ever.jpeg", "fall_asleep.png");
 
-var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp", "red.png");
+var pics2 = new Array("am.jpg", "humbug.jpg", "whatever_people_say.webp", "red.png", "music_murder.png", "sour.png", "punisher.jpeg");
 
-var pics3 = new Array("in_utero.jpg", "nevermind.webp", "meteora.jpg", "fearless.png");
+var pics3 = new Array("in_utero.jpg", "nevermind.webp", "meteora.jpg", "fearless.png", "tickets_downfall.png", "flowerboy.png", "american_teen.png");
 
-var most_popular = new Array("in utero", "nevermind", "meteora", "red");
+var most_popular_ids = new Array("image3", "image3", "image3", "image2", "image1", "image2","image1");
 
-var most_popular_ids = new Array("image3", "image3", "image3", "image2");
+var wrong_ids_1 = new Array("image1", "image1", "image1", "image1", "image2", "image1","image2");
 
-var most_popular_sounds = new Array("heart_box.mp3", "nirvana.mp3", "numb.mp3", "red.mp3")
+var wrong_ids_2 = new Array("image2", "image2", "image2", "image3", "image3", "image3","image3");
 
-var random = Math.floor(Math.random()*4);
+var most_popular_sounds = new Array("heart_box.mp3", "nirvana.mp3", "numb.mp3", "red.mp3", "thunder.mp3", "drivers_license.mp3","badguy.mp3");
 
-function choosePic(img, p){
-  document.getElementById(img).src = p[random];
+var random = Math.floor(Math.random()*7);
+
+function generate(){
+  choosePic("image1", pics1, random);
+  choosePic("image2", pics2, random);
+  choosePic("image3", pics3, random);
+}
+
+function choosePic(img, p, i){
+  document.getElementById(img).src = p[i];
 }
 
 function myFunction() {
-  var most_pop = most_popular[random];
   var most_pop_sound = most_popular_sounds[random];
   var element = document.getElementById(most_popular_ids[random]);
   element.style.borderColor = "#00FF00";
+  var element2 = document.getElementById(wrong_ids_1[random]);
+  var element3 = document.getElementById(wrong_ids_2[random]);
+  element2.style.borderColor = "#FF0000";
+  element3.style.borderColor = "#FF0000";
   var snd = new Audio(most_popular_sounds[random]);
   snd.play();
-  alert(most_pop + ' was the most popular');
+  document.getElementById("refresh").innerHTML = "Refresh the page to play again!"
 }
 
-choosePic("image1", pics1);
-choosePic("image2", pics2);
-choosePic("image3", pics3);
+
